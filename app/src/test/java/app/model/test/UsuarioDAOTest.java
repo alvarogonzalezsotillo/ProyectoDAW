2939970bd0036dc198c4804d23ebe43433d1fc6c
guillermo <gmuela92@gmail.com>
2016-04-16 18:27:29 +0200

    UsuarioDO terminado|Probado con test de integración(warning solucionado)

--- app/src/test/java/app/model/test/UsuarioDAOTest.java
+++ app/src/test/java/app/model/test/UsuarioDAOTest.java
@@ -7,7 +7,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import app.beans.imp.UsuarioImp;
-import app.builder.UsuarioBuilder;
 import app.model.UsuarioDAO;
 
 public class UsuarioDAOTest {
