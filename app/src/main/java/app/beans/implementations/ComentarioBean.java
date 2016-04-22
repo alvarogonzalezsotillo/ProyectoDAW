4e82af1ad91bb9c00578cf0c854b9bd712b30c4f
guillermo <gmuela92@gmail.com>
2016-04-22 21:52:53 +0200

    Creados ComentarioBean y Builder

--- app/src/main/java/app/beans/implementations/ComentarioBean.java
+++ app/src/main/java/app/beans/implementations/ComentarioBean.java
@@ -2,13 +2,67 @@ package app.beans.implementations;
 
 import app.beans.interfaces.Comentario;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "comentarios")
-public class ComentarioBean implements Comentario{
+public class ComentarioBean implements Comentario, Serializable {
 
+    private static final long serialVersionUID = 1L;
 
+    @Column(name = "id")
+    private Long id;
 
+    @Column(name = "texto")
+    private String texto;
+
+    @Column(name = "idUsuario")
+    private Long idUsuario;
+    @Column(name = "idTweet")
+    private Long idTweet;
+
+    public ComentarioBean() {
+        //Constructor por defecto
+    }
+
+    public ComentarioBean(String texto, Long idUsuario, Long idTweet) {
+        this.texto = texto;
+        this.idUsuario = idUsuario;
+        this.idTweet = idTweet;
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Long getIdTweet() {
+        return idTweet;
+    }
+
+    public void setIdTweet(Long idTweet) {
+        this.idTweet = idTweet;
+    }
 }
