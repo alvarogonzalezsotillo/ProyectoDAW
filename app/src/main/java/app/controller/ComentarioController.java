cd32626d6d7decd5c50280bcfde5eb03a4878031
jordc <jor.cp.android@gmail.com>
2016-04-25 20:42:13 +0200

    ComentarioController y comentarioForm.xhtml

---
+++ app/src/main/java/app/controller/ComentarioController.java
@@ -0,0 +1,78 @@
+package app.controller;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+import app.beans.implementations.ComentarioBean;
+import app.builder.ComentarioBuilder;
+import app.model.ComentarioDAO;
+
+@ManagedBean(name = "comentarioController")
+@ViewScoped
+public class ComentarioController implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private ComentarioBuilder comentarioBuilder;
+
+	private String texto;
+    private Long idUsuario;
+    private Long idTweet;
+	
+    @ManagedProperty(value = "#{ComentarioDAO}")
+    private ComentarioDAO comentarioDao;
+    
+    public void insertComentario() {
+	    comentarioBuilder = new ComentarioBuilder(texto, idUsuario, idTweet);
+	    	
+	    ComentarioBean comentario = comentarioBuilder.build();
+	    	
+	    comentarioDao.insertComentario(comentario);
+    	
+    }
+    
+	public ComentarioBuilder getComentarioBuilder() {
+		return comentarioBuilder;
+	}
+
+	public void setComentarioBuilder(ComentarioBuilder comentarioBuilder) {
+		this.comentarioBuilder = comentarioBuilder;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public Long getIdTweet() {
+		return idTweet;
+	}
+
+	public void setIdTweet(Long idTweet) {
+		this.idTweet = idTweet;
+	}
+
+	public ComentarioDAO getComentarioDao() {
+		return comentarioDao;
+	}
+
+	public void setComentarioDao(ComentarioDAO comentarioDao) {
+		this.comentarioDao = comentarioDao;
+	}
+	
+	
+}
