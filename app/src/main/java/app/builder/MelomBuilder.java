30159ec826b8c3e10a6fdbf1e197f42a0a005f23
jordc <jor.cp.android@gmail.com>
2016-04-29 23:18:41 +0200

    Implementación del MelomBuilder.java

---
+++ app/src/main/java/app/builder/MelomBuilder.java
@@ -0,0 +1,100 @@
+package app.builder;
+
+import java.io.Serializable;
+
+import org.primefaces.model.UploadedFile;
+
+import app.beans.implementations.MelomBean;
+import app.utils.Util;
+
+public class MelomBuilder implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String titulo;
+	private String album;
+	private String tipoMusica;
+	private String comentarioMusico;
+	private byte[] cancion;
+	private byte[] imagenAlbum;
+	private Long idUsuario;
+	
+	public MelomBuilder(){
+        
+        //Constructor por defecto para los test
+        
+    }
+	
+	public MelomBuilder(String titulo, String album, String tipoMusica, String comentarioMusico, byte[] cancion, byte[] imagenAlbum, Long idUsuario){
+		this.titulo = titulo;
+		this.album = album;
+		this.tipoMusica = tipoMusica;
+		this.comentarioMusico = comentarioMusico;
+		this.cancion = cancion;
+		this.imagenAlbum = imagenAlbum;
+		this.idUsuario = idUsuario;
+    }
+
+    public MelomBean build() {
+
+        return new MelomBean(titulo, album, tipoMusica, comentarioMusico, cancion, imagenAlbum, idUsuario);
+
+    }
+    
+    public MelomBuilder titulo(String titulo) {
+
+        this.titulo = titulo;
+
+        return this;
+
+    }
+    
+    public MelomBuilder album(String album) {
+
+        this.album = album;
+
+        return this;
+
+    }
+    
+    public MelomBuilder tipoMusica(String tipoMusica) {
+
+        this.tipoMusica = tipoMusica;
+
+        return this;
+
+    }
+    
+    public MelomBuilder comentarioMusico(String comentarioMusico) {
+
+        this.comentarioMusico = comentarioMusico;
+
+        return this;
+
+    }
+    
+    public MelomBuilder cancion(UploadedFile cancion) {
+
+        this.cancion = Util.transformFile(cancion);
+
+        return this;
+
+    }
+    
+    public MelomBuilder imagenAlbum(UploadedFile imagenAlbum) {
+
+        this.imagenAlbum = Util.transformFile(imagenAlbum);
+
+        return this;
+
+    }
+    
+    public MelomBuilder idUsuario(Long idUsuario) {
+
+        this.idUsuario = idUsuario;
+
+        return this;
+
+    }
+    
+}
