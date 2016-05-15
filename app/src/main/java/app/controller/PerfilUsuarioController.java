5919e75fc625b7b8646356a63fc9df122ce048b8
guillermo <gmuela92@gmail.com>
2016-05-15 19:56:25 +0200

    Añadido primer controlador para recuperar datos de BD. Falta la imagen

---
+++ app/src/main/java/app/controller/PerfilUsuarioController.java
@@ -0,0 +1,152 @@
+package app.controller;
+
+import app.beans.UsuarioBean;
+import app.controller.interfaces.Controller;
+import app.model.UsuarioDAO;
+import app.utils.UtilSessionHibernate;
+import org.hibernate.Session;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import java.io.ByteArrayInputStream;
+import java.io.Serializable;
+
+@ManagedBean(name = "perfilUsuarioController")
+@ViewScoped
+public class PerfilUsuarioController implements Controller, Serializable {
+
+    @ManagedProperty(value = "#{usuarioDao}")
+    private UsuarioDAO usuarioDao;
+
+    private String nombre;
+    private String apellido;
+    private String nick;
+    private String correo;
+    private String grupo;
+    private String web;
+    private String tipoMusica;
+    private String fechaRegistro;
+    private StreamedContent imagen;
+
+    public void init(){
+
+        initSessionForDao();
+        UsuarioBean usuarioActual = usuarioDao.getById(1L);
+        closeSession();
+
+        this.nombre = usuarioActual.getNombreDeUsuario();
+        this.apellido = usuarioActual.getApellidoDeUsuario();
+        this.nick = usuarioActual.getNickDeUsuario();
+        this.correo = usuarioActual.getCorreoDeUsuario();
+        this.grupo = usuarioActual.getGrupoDeUsuario();
+        this.web = usuarioActual.getWebDeUsuario();
+        this.tipoMusica = usuarioActual.getTipoMusicaDeUsuario();
+        this.imagen = new DefaultStreamedContent(new ByteArrayInputStream(usuarioActual.getImagenDeUsuario()));
+
+    }
+
+    public void initSessionForDao(){
+        Session session = UtilSessionHibernate.initSession();
+        usuarioDao.setSession(session);
+    }
+
+    public void commitAndCloseSession(){
+        Session session = usuarioDao.getSession();
+        UtilSessionHibernate.commitAndCloseSession(session);
+
+    }
+
+
+    public void initTransactionForDao(){
+        Session session = usuarioDao.getSession();
+        UtilSessionHibernate.initTransaction(session);
+
+    }
+
+
+    public void closeSession(){
+        Session session = usuarioDao.getSession();
+        UtilSessionHibernate.closeSession(session);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public UsuarioDAO getUsuarioDao() {
+        return usuarioDao;
+    }
+
+    public void setUsuarioDao(UsuarioDAO usuarioDao) {
+        this.usuarioDao = usuarioDao;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+    public String getTipoMusica() {
+        return tipoMusica;
+    }
+
+    public void setTipoMusica(String tipoMusica) {
+        this.tipoMusica = tipoMusica;
+    }
+
+    public String getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(String fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public StreamedContent getImagen() {
+
+        return imagen;
+
+    }
+}
